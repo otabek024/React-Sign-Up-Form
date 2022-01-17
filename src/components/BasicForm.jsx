618399e7e3eb845e5e3654a2053ee945b79c4b59
OtabekAbdurakhmonov <77769473+OtabekAbdurakhmonov@users.noreply.github.com>
2022-01-17 18:41:59 +0900

    changes

---
+++ src/components/BasicForm.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+import UseInput from "../hooks/UseInput";
+import "../App.css";
+
+const isNotEmpty = (value) => /^[A-Za-z ,.'-]+$/.test(value);
+const isEmail = (value) => /\S+@\S+\.\S+/.test(value);
+const isPassword = (value) =>
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(value);
+
+const isConfirmPassword = (value) => {
+  if (value.passwordValue == value.confirmPasswordValue) {
+    console.log("error");
+  }
+};
+
+const BasicForm = (props) => {
+  const {
+    value: userNameValue,
+    isValid: userNameIsValid,
+    hasError: userNameHasError,
+    valueChangeHandler: userNameChangeHandler,
+    inputBlueHandler: userNameBlurHandler,
+    reset: resetUserName,
+  } = UseInput(isNotEmpty);
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlueHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = UseInput(isEmail);
+
+  const {
+    value: passwordValue,
+    isValid: passwordIsValid,
+    hasError: passwordHasError,
+    valueChangeHandler: passwordChangeHandler,
+    inputBlueHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = UseInput(isPassword);
+
+  const {
+    value: confirmPasswordValue,
+    isValid: confirmPasswordIsValid,
+    hasError: confirmPasswordHasError,
+    valueChangeHandler: confirmPasswordChangeHandler,
+    inputBlueHandler: confirmPasswordBlurHandler,
+    reset: resetConfirmPassword,
+  } = UseInput(isConfirmPassword);
+
+  let formIsValid = false;
+
+  if (
+    userNameIsValid &&
+    emailIsValid &&
+    passwordIsValid &&
+    confirmPasswordIsValid
+  ) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log("Submitted");
+    console.log(userNameValue, emailValue, passwordValue, confirmPasswordValue);
+
+    resetUserName();
+    resetEmail();
+    resetPassword();
+    resetConfirmPassword();
+  };
+
+  const userNameClasses = userNameHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
+
+  const passwordClasses = passwordHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  const confirmPasswordClasses = confirmPasswordHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  return (
+    <form onSubmit={submitHandler}>
+      <h1>Sign Up</h1>
+      <div className="control-group">
+        <div className={userNameClasses}>
+          <label htmlFor="name">Username</label>
+          <input
+            type="text"
+            id="name"
+            value={userNameValue}
+            onChange={userNameChangeHandler}
+            onBlur={userNameBlurHandler}
+          />
+          {userNameHasError && (
+            <p className="error-text">Looks like invalid username</p>
+          )}
+        </div>
+        <div className={emailClasses}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={emailValue}
+            onChange={emailChangeHandler}
+            onBlur={emailBlurHandler}
+          />
+          {emailHasError && (
+            <p className="error-text">Looks like invalid email address</p>
+          )}
+        </div>
+        <div className={passwordClasses}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={passwordValue}
+            onChange={passwordChangeHandler}
+            onBlur={passwordBlurHandler}
+          />
+          {passwordHasError && (
+            <p className="error-text">Looks like invalid password</p>
+          )}
+        </div>
+        <div className={confirmPasswordClasses}>
+          <label htmlFor="password2">Confirm Password</label>
+          <input
+            type="password"
+            id="password2"
+            value={confirmPasswordValue}
+            onChange={confirmPasswordChangeHandler}
+            onBlur={confirmPasswordBlurHandler}
+          />
+          {confirmPasswordHasError && (
+            <p className="error-text">Passwords do not match!</p>
+          )}
+        </div>
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default BasicForm;
